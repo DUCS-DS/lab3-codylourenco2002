a5b9b61e59217bb92ea0dffd452b367f35e9a421
Codylourenco2002 <clourenco@drury.edu>
2024-03-02 15:02:24 -0600

    Challenge

--- cyclic.py
+++ cyclic.py
@@ -2,13 +2,59 @@ from llist import *
 
 def llprint(lst, num):
     """print the first num terms of linked list lst"""
-    pass
+    current = lst.head
+    for _ in range(num):
+        if current:
+            print(current.val, end=" ")
+            current = current.next
+        else:
+            break
+    print()
 
 def find_cycle(lst):
     """return the start index and length of the cycle"""
-    pass
+    slow = fast = lst.head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    else:
+        return None  # No cycle
 
+    # Find the start of the cycle
+    slow = lst.head
+    start_index = 0
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+        start_index += 1
+
+    # Find the length of the cycle
+    cycle_length = 1
+    fast = fast.next
+    while slow != fast:
+        fast = fast.next
+        cycle_length += 1
+
+    return start_index, cycle_length
 
 if __name__ == "__main__":
+    values = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
+    linked_list = LList()
+    for value in values:
+        append(linked_list, Node(value))
 
-    pass
+    # Create a cycle of length 5 that starts at position 10
+    node = linked_list.head
+    for _ in range(10):
+        node = node.next
+    cycle_start = node
+
+    node = linked_list.head
+    while node.next:
+        node = node.next
+    node.next = cycle_start
+
+    # Print the first 30 terms of the linked list
+    llprint(linked_list, 30)
--- finite.py
+++ finite.py
@@ -1,20 +1,33 @@
+#Cody Lourenco
 from llist import LList, Node, append
-
 
 def length(lst):
     """return the length of a finite linked list"""
-    pass  # delete this line when you add your code
-
+    current = lst.head
+    count = 0
+    while current:
+        count += 1
+        current = current.next
+    return count
 
 def llprint(lst):
     """print a finite linked list"""
-    pass  # delete this line when you add your code
-
+    current = lst.head
+    while current:
+        print(current.val, end=" ")  # Note: changed from current.data to current.val
+        current = current.next
+    print()
 
 if __name__ == "__main__":
+    values = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+    linked_list = LList()
+    for value in values:
+        append(linked_list, Node(value))
 
-    pass  # delete this line when you add your code below
+    print("Length of linked list:", length(linked_list))
+    print("Linked list:")
+    llprint(linked_list)
 
-    #
-    # your tests go here
-    #
+    # Exercise 5
+    from genfinite import lst
+    print(length(lst))
